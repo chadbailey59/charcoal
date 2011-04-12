436cad4fbc3ba035f0d22a7054daaba305ff0ede
Chad Bailey <chad.bailey@lifechurch.tv>
2011-04-11 23:55:52 -0500

    added gemfile

--- charcoal.rb
+++ charcoal.rb
@@ -5,6 +5,6 @@ require 'yaml'
 require File.join(File.expand_path(File.dirname(__FILE__)), "slug.rb")
 
 get '/all' do
-  @data = YAML.load_file "#{File.expand_path(File.dirname(__FILE__))}/config.yml"
+  @data = YAML.load_file "config.yml"
   haml :stats, :locals => {:data => @data}
 end
